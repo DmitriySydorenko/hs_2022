1f30406a7c21711c53072f6f555d65a4e19076ae
Dmitry <dmitry.vitalievich@gmail.com>
2023-01-25 23:18:22 +0200

    class Select

--- app/controllers/models/Gallery.php
+++ app/controllers/models/Gallery.php
@@ -2,13 +2,20 @@
 
 namespace App\controllers\models;
 
+
 class Gallery {
     public function index() {
 
-        $dbh = new \PDO('mysql:host=localhost;dbname=testBase', 'root', 'root');
-        $sth = $dbh->query("SELECT * FROM Users");
-        $rows = $sth->fetchAll(\PDO::FETCH_ASSOC);
+        // $dbh = new \PDO('mysql:host=localhost;dbname=testBase', 'root', 'root');
+        // $sth = $dbh->query("SELECT * FROM Users");
+        // $rows = $sth->fetchAll(\PDO::FETCH_ASSOC);
 
-        return $rows;
+        // return $rows;
+
+        $select = new \Core\Select();
+        $select-> setTableName('Users');
+        return $select->execute();
+
     }
 }
+
